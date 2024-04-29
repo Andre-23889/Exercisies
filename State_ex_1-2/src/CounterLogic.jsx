5804c7fc5a69ea5b48d1cfca62195d0140864d99
Fabio Audisio <fabio.audisio@itiscuneo.eu>
2024-04-29 03:33:24 +0200

    update
    add the exercise State_ex_1-2

---
+++ State_ex_1-2/src/CounterLogic.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import { CounterDisplay } from './CounterDisplay'
+
+function CounterLogic({ initialValue= 0, incrementBy= 1, decrementBy= 1 }) {
+	const [counter, setCounter] = useState(initialValue)
+    const count= counter
+
+	function eventIncrement() {
+		setCounter((c) => c + incrementBy)
+	}
+
+	function eventDecrement() {
+		setCounter((c) => c - decrementBy)
+	}
+
+	function eventReset() {
+		setCounter((c)=> c= initialValue)
+	}
+
+	return (
+		<div>
+			<CounterDisplay counter={counter} />
+			<button onClick={eventIncrement}>Increment</button>
+			<button onClick={eventDecrement}>Decrement</button>
+			<button onClick={eventReset}>Reset</button>
+		</div>
+	)
+}
+
+createRoot(document.getElementById("root")).render(<CounterLogic />)
+
+
+/* è consigliabile scrivere una callback function in setCounter, per una serie di motivi:
+ lo hook è assegnato ad una costante, il cui valore 
+ quindi non può essere modificato direttamente ma attraverso una funzione raccolta nella costante stessa.
+ Inoltre usando delle callback possiamo effettuare anche più operazioni che nel momento del rendering, perché queste
+ verranno aggiunte allo stack delle operazioni: quindi saranno esegiute una dopo l'atra rendendo alla successiva un valore aggiornato*/
